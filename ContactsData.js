62a53b26a520d2f9a4064ee4d8a2a1f8f5907fe9
Vika Shelestenko <viktoria.shelestenko@sigma.software>
2018-12-05 12:02:10 +0200

    Ready test project

---
+++ ContactsData.js
@@ -0,0 +1,30 @@
+var contacts = [
+    {
+        "name": "Ross Geller",
+        "phone": "012-32343253",
+        "email": "we_were_on_a_break@dinasours.com",
+        "nickname": "unagi",
+        "favorite": "true"
+
+    },
+    {
+        "name":"Phoebe Buffay",
+        "phone": "012-10000000",
+        "email": "smelly_cat@gmail.com",
+        "nickname": "Princess Consuela Bananahammock",
+        "favorite": "false"
+    },
+    {
+        "name":"Joey Tribbiani",
+        "phone": "012-123423543",
+        "email": "joey@doesnt-share-food.com",
+        "nickname": "Baby Kangaroo Tribbiani",
+        "favorite": "true"
+    }
+
+];
+
+function myFunc(firstParam, second) {
+
+}
+
